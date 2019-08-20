3a0de3bf5424b6132538a4273310102464d8df4d
rhys <rhys.shea.10@ucl.ac.uk>
2019-08-20 18:40:35 +0900

    network algorithms

---
+++ Data_Structures/week1_basic_data_structures/my_packet_processing.cpp
@@ -0,0 +1,195 @@
+#include <iostream>
+#include <cassert>
+#include <random>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <sys/time.h>
+#include <csignal>
+#include <queue>
+
+using std::vector;
+using std::string;
+using namespace std;
+
+/*
+Implement a program to simulate the processing of network packets
+*/
+
+struct Request {
+    Request(int arrival_time, int process_time):
+        arrival_time(arrival_time),
+        process_time(process_time)
+    {}
+
+    int arrival_time;
+    int process_time;
+};
+
+struct Response {
+    Response(bool dropped, int start_time):
+        dropped(dropped),
+        start_time(start_time)
+    {}
+
+    bool dropped;
+    int start_time;
+};
+
+class Buffer {
+public:
+    Buffer(int size):
+        size_(size),
+        finish_time_()
+    {}
+
+    Response Process(const Request &request) {
+        // write your code here
+        // if finish time has values then check the front finish time until it is greater than the current arrival time
+        // cout << "size: " << finish_time_.size() << "\n";
+        // cout << "A\n";
+        bool check = true;
+        if (finish_time_.empty() == false) {
+          while (check) {
+            if (finish_time_.front() <= request.arrival_time) {
+              finish_time_.pop();
+              if (finish_time_.empty()) {
+                check = false;
+              }
+            } else {
+              check = false;
+            }
+          }
+        }
+
+        if (finish_time_.empty() == false){
+          if (finish_time_.size() >= size_) {
+            return Response(true, request.arrival_time);
+          } else {
+            finish_time_.push(finish_time_.back() + request.process_time);
+
+            return Response(false, finish_time_.back() - request.process_time);
+          }
+        } else {
+          finish_time_.push(request.arrival_time + request.process_time);
+          return Response(false, request.arrival_time);
+        }
+
+        return Response(false, 1);
+
+    }
+private:
+    int size_;
+    queue <int> finish_time_;
+};
+
+vector <Request> ReadRequests() {
+    vector <Request> requests;
+    int count;
+    cin >> count;
+    for (int i = 0; i < count; ++i) {
+        int arrival_time, process_time;
+        cin >> arrival_time >> process_time;
+        requests.push_back(Request(arrival_time, process_time));
+    }
+    return requests;
+}
+
+vector <Response> ProcessRequests(const vector <Request> &requests, Buffer *buffer) {
+    vector <Response> responses;
+    for (int i = 0; i < requests.size(); ++i) {
+
+      responses.push_back(buffer->Process(requests[i]));
+    }
+    return responses;
+}
+
+void PrintResponses(const vector <Response> &responses) {
+    for (int i = 0; i < responses.size(); ++i)
+        cout << (responses[i].dropped ? -1 : responses[i].start_time) << endl;
+}
+
+
+/*
+submission function
+*/
+
+int main() {
+    int size;
+    cin >> size;
+    vector <Request> requests = ReadRequests();
+
+    Buffer buffer(size);
+    vector <Response> responses = ProcessRequests(requests, &buffer);
+
+    PrintResponses(responses);
+    return 0;
+}
+
+
+/*
+test functions
+*/
+// int main() {
+//
+//   int n = generate(100000);
+//   vector<int> a(n);
+//   for (size_t i = 0; i < a.size(); ++i) {
+//     a[i] = generate(1000000000);
+//   }
+//
+//   struct timespec start, end;
+//
+//   // start time
+//   clock_gettime(CLOCK_MONOTONIC, &start);
+//   // function call
+//   test_solution(a, 0, a.size());
+//   // end time
+//   clock_gettime(CLOCK_MONOTONIC, &end);
+//   // calculate and print time
+//   double time_taken = (end.tv_sec - start.tv_sec) * 1e9;
+//   time_taken = (time_taken + (end.tv_nsec - start.tv_nsec)) * 1e-9;
+//   cout << "n: " << n << "\n";
+//   cout << "Time taken: " << fixed << time_taken;
+//   cout << "sec" << endl;
+// }
+
+
+/*
+stress testing
+*/
+// int main() {
+//   struct timespec start, end;
+//
+//   while (true) {
+//     int n = generate(100000);
+//     vector<int> a(n);
+//     for (size_t i = 0; i < a.size(); ++i) {
+//       a[i] = generate(1000000000);
+//     }
+//
+//     // base solution timing and running
+//     clock_gettime(CLOCK_MONOTONIC, &start);
+//     int solution_base = base_solution(a, 0, a.size());
+//     clock_gettime(CLOCK_MONOTONIC, &end);
+//     double time_takenB = (end.tv_sec - start.tv_sec) * 1e9;
+//     time_takenB = (time_takenB + (end.tv_nsec - start.tv_nsec)) * 1e-9;
+//
+//     // test solution timing and running
+//     clock_gettime(CLOCK_MONOTONIC, &start);
+//     int solution_test = test_solution(a, 0, a.size());
+//     clock_gettime(CLOCK_MONOTONIC, &end);
+//     double time_takenT = (end.tv_sec - start.tv_sec) * 1e9;
+//     time_takenT = (time_takenT + (end.tv_nsec - start.tv_nsec)) * 1e-9;
+//
+//     if (solution_base != solution_test) {
+//       cout << "Input: " << n << "\n";
+//       cout << "Base: " << solution_base << " Test: " << solution_test << "\n";
+//       // break;
+//     }
+//     cout << "Input: " << n << "\n";
+//     cout << "Base: " << solution_base << " Test: " << solution_test << "\n";
+//     cout << "Base time: " << fixed << time_takenB << "  Test time: " << fixed << time_takenT << "\n";
+//     cout << "\n";
+//   }
+// }
